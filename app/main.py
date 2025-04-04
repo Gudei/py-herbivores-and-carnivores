2853a7c8daa28e0e33368a99f177947c169d3b72
Illya Huda <twixer41@gmail.com>
2025-04-04 11:21:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,20 +1,19 @@
 class Animal:
     alive = []
 
-    def __init__(
-            self, name: str, health: int = 100, hidden: bool = False
-    ) -> None:
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        return (f"{{"
-                f"Name: {self.name},"
+        return (f"{{Name: {self.name},"
                 f" Health: {self.health},"
-                f" Hidden: {self.hidden}"
-                f"}}")
+                f" Hidden: {self.hidden}}}")
 
     def die(self) -> None:
         if self in Animal.alive:
